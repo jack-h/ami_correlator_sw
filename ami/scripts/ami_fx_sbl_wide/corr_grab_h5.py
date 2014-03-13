e822752ad97a674b7c10fe261bff2105531ab0e1
AMI Operator <ami@appcl.(none)>
2014-03-13 10:57:17 +0000

    Catch kill signals in corr_grab_h5.py
    Previously, KeyboardInterrupt triggered a graceful exit, but
    the kill signal initiated by amisa over ssh didn't, leading to corrupt
    files.
    This commit adds catching of the SIGINT and SIGTERM signals, using
    the signal module. A timeout has also been added to halt
    data collection when no new metadata has been received in the last
    10 minutes

--- ami/scripts/ami_fx_sbl_wide/corr_grab_h5.py
+++ ami/scripts/ami_fx_sbl_wide/corr_grab_h5.py
@@ -9,6 +9,7 @@ import ami.helpers as helpers
 import ami.amisa_control as control
 import ami.file_writer as fw
 import pylab
+import signal
 
 def write_data(writer, d, timestamp, meta):
     for entry in meta.entries:
@@ -25,6 +26,15 @@ def write_data(writer, d, timestamp, meta):
            writer.append_data(name, [length], val, data_type)
     writer.append_data('xeng_raw0', d.shape, d, np.int64)
     writer.append_data('timestamp0', [1], timestamp, np.int64)
+
+def signal_handler(signum, frame):
+    """
+    Run when kill signals are caught
+    """
+    print "Received kill signal %d. Closing files and exiting"%signum
+    writer.close_file()
+    ctrl.close_sockets()
+    exit()
 
 
 if __name__ == '__main__':
@@ -64,74 +74,78 @@ if __name__ == '__main__':
     time.sleep(0.1)
 
     xeng = corr.xengs[0]
+
+    # some initial values for the loop
     cnt=0
     datavec = np.zeros([corr.n_chans,corr.n_bls,corr.n_pols,2],dtype=np.int64)
     current_obs = None
     mcnt_old = xeng.read_uint('mcnt_lsb')
     receiver_enable = False
     scale = None
+    last_meta_timestamp = time.time()
+    # Catch keyboard interrupt and kill signals (which are initiated by amisa over ssh)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
     while(True):
-        try:
-            if (ctrl.try_recv()==0):
-                print "received metadata with timestamp", ctrl.meta_data.timestamp
-                receiver_enable = (ctrl.meta_data.obs_status==4)
-                if not receiver_enable:
-                    print "OBS NOT ACTIVE. CLOSING FILES"
-                    #set current obs to none so the next valid obs will trigger a new file
-                    current_obs = None
-                    writer.close_file()
-                elif ctrl.meta_data.obs_name != current_obs:
-                    writer.close_file()
-                    fname = 'corr_%s_%d.h5'%(ctrl.meta_data.obs_name, ctrl.meta_data.timestamp)
-                    if not opts.test_tx:
-                        print "Starting a new file with name", fname
-                        writer.start_new_file(fname)
-                        writer.add_attr('obs_name',ctrl.meta_data.obs_name)
-                    current_obs = ctrl.meta_data.obs_name
-            if receiver_enable:
-                mcnt = xeng.read_uint('mcnt_lsb')
-                if mcnt != mcnt_old:
-                    mcnt_old = mcnt
-                    d = corr.snap_corr(wait=False,combine_complex=False)
-                    cnt += 1
-                    if d is not None:
-                        datavec[:,0,0,1] = d['corr00']
-                        datavec[:,1,0,1] = d['corr11']
-                        datavec[:,2,0,1] = d['corr01'][0::2] #datavec[:,:,:,1] should be real
-                        datavec[:,2,0,0] = d['corr01'][1::2] #datavec[:,:,:,0] should be imag
-                        print "got new correlator data with timestamp",d['timestamp']
-                        maxd = np.max(np.abs(d['corr01']))
-                        if scale is None:
-                            scale = 2.**31 / np.mean(np.abs(d['corr01'])) / 1000.
-
-                        print "max data value is %d (%f bits) (scaled by %f)"%(np.round(maxd*scale,0),np.log2(maxd*scale),scale)
-                        txdata = d['corr01']*scale
-                        #saturate
-                        txdata[txdata>(2**31-1)] = 2**31 - 1
-                        txdata[txdata<-(2**31)] = -(2**31)
-                        txdata = np.array(np.round(txdata,0),dtype=np.int32)
-
-                        #for datan,data in enumerate(txdata):
-                        #    print "Sending data. Index %4d, %d"%(datan,data)
-
-                        if not opts.test_tx:
-                            ctrl.try_send(d['timestamp'],1,cnt,txdata)
-                            write_data(writer,datavec,d['timestamp'],ctrl.meta_data)
-                            #pylab.plot(helpers.dbs(np.abs(txdata)))
-                            #pylab.show()
-                            #exit()
-                        else:
-                            fake_data = np.arange(4096)+cnt
-                            ctrl.try_send(d['timestamp'],1,cnt,fake_data)
-                    else:
-                        print "Failed to send because MCNT changed during snap"
-                    #cnt += 1
-        except KeyboardInterrupt:
-            print 'Received keyboard interrupt. Closing files and exiting'
+        if (ctrl.try_recv()==0):
+            print "received metadata with timestamp", ctrl.meta_data.timestamp
+            last_meta_timestamp = ctrl.meta_data.timestamp
+            receiver_enable = (ctrl.meta_data.obs_status==4)
+            if not receiver_enable:
+                print "OBS NOT ACTIVE. CLOSING FILES"
+                #set current obs to none so the next valid obs will trigger a new file
+                current_obs = None
+                writer.close_file()
+            elif ctrl.meta_data.obs_name != current_obs:
+                writer.close_file()
+                fname = 'corr_%s_%d.h5'%(ctrl.meta_data.obs_name, ctrl.meta_data.timestamp)
+                if not opts.test_tx:
+                    print "Starting a new file with name", fname
+                    writer.start_new_file(fname)
+                    writer.add_attr('obs_name',ctrl.meta_data.obs_name)
+                current_obs = ctrl.meta_data.obs_name
+        if (time.time() - last_meta_timestamp) > 60*10:
+            print "10 minutes has elapsed since last valid meta timestamp"
+            print "Closing Files"
+            #set current obs to none so the next valid obs will trigger a new file
+            current_obs = None
             writer.close_file()
-            ctrl.close_sockets()
-            exit()
+            receiver_enable = False # disable data capture until new meta data arrives
+        if receiver_enable:
+            mcnt = xeng.read_uint('mcnt_lsb')
+            if mcnt != mcnt_old:
+                mcnt_old = mcnt
+                d = corr.snap_corr(wait=False,combine_complex=False)
+                cnt += 1
+                if d is not None:
+                    datavec[:,0,0,1] = d['corr00']
+                    datavec[:,1,0,1] = d['corr11']
+                    datavec[:,2,0,1] = d['corr01'][0::2] #datavec[:,:,:,1] should be real
+                    datavec[:,2,0,0] = d['corr01'][1::2] #datavec[:,:,:,0] should be imag
+                    print "got new correlator data with timestamp",d['timestamp']
+                    maxd = np.max(np.abs(d['corr01']))
+                    if scale is None:
+                        scale = 2.**31 / np.mean(np.abs(d['corr01'])) / 1000.
+
+                    print "max data value is %d (%f bits) (scaled by %f)"%(np.round(maxd*scale,0),np.log2(maxd*scale),scale)
+                    txdata = d['corr01']*scale
+                    #saturate
+                    txdata[txdata>(2**31-1)] = 2**31 - 1
+                    txdata[txdata<-(2**31)] = -(2**31)
+                    txdata = np.array(np.round(txdata,0),dtype=np.int32)
+
+                    #for datan,data in enumerate(txdata):
+                    #    print "Sending data. Index %4d, %d"%(datan,data)
+
+                    if not opts.test_tx:
+                        ctrl.try_send(d['timestamp'],1,cnt,txdata)
+                        write_data(writer,datavec,d['timestamp'],ctrl.meta_data)
+                        #pylab.plot(helpers.dbs(np.abs(txdata)))
+                        #pylab.show()
+                        #exit()
+                    else:
+                        fake_data = np.arange(4096)+cnt
+                        ctrl.try_send(d['timestamp'],1,cnt,fake_data)
+                else:
+                    print "Failed to send because MCNT changed during snap"
         time.sleep(0.1)
-
-
-
