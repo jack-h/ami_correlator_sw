79890146ec62310a9c37a0b076891a1fc25ba384
AMI Operator <ami@amisdc.(none)>
2015-03-19 17:18:59 +0000

    Add options to snap multiple time. Plot gaussian fits.

--- ami/scripts/ami_fx/corr_snap_adc.py
+++ ami/scripts/ami_fx/corr_snap_adc.py
@@ -9,6 +9,12 @@ import ami.helpers as helpers
 import struct
 import scipy.stats
 
+def add_gauss_fit(data):
+    mean = np.mean(data)
+    sigma = np.sqrt(np.var(data))
+    x = np.linspace(min(data), max(data), 100)
+    pylab.plot(x,pylab.normpdf(x,mean,sigma))
+
 if __name__ == '__main__':
     from optparse import OptionParser
 
@@ -17,6 +23,10 @@ if __name__ == '__main__':
     p.set_description(__doc__)
     p.add_option('-c', '--per_core', dest='per_core',action='store_true', default=False,
         help='Plot histograms of each ADC core')
+    p.add_option('-b', '--histbins', dest='histbins', type='int', default=2**6,
+        help='Number of histogram bins. Default = 2**6')
+    p.add_option('-n', '--nsnaps', dest='nsnaps', type='int', default=1,
+        help='Number of 16ksample chunks to snap before doing stats. Default:1')
 
     opts, args = p.parse_args(sys.argv[1:])
 
@@ -37,7 +47,9 @@ if __name__ == '__main__':
     y_plots = int(np.ceil(n_plots / float(x_plots)))
     stddevs = {}
     for fn,feng in enumerate(corr.fengs):
-        adc = feng.snap('snapshot_adc', man_trig=True, format='b', wait_period=1)
+        adc = np.array([])
+        for n in range(opts.nsnaps):
+            adc = np.append(adc, feng.snap('snapshot_adc', man_trig=True, format='b', wait_period=1))
         pylab.figure(0)
         pylab.subplot(x_plots,y_plots,fn+1)
         pylab.plot(adc)
@@ -45,38 +57,27 @@ if __name__ == '__main__':
         pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
         pylab.figure(1)
         pylab.subplot(x_plots,y_plots,fn+1)
-        pylab.hist(adc, bins=2**5, range=(-2**7, 2**7), normed=True)
+        pylab.hist(adc, bins=opts.histbins, range=(-2**7, 2**7), normed=True)
         pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
+        add_gauss_fit(adc)
 
         if opts.per_core:
-            pylab.figure(2)
-            pylab.suptitle('Core 0')
-            pylab.subplot(x_plots,y_plots,fn+1)
-            pylab.hist(adc[0::4], bins=2**5, range=(-2**7, 2**7), normed=True)
-            pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
+            for i in range(4):
+                pylab.figure(2+i)
+                pylab.suptitle('Core %d'%i)
+                pylab.subplot(x_plots,y_plots,fn+1)
+                pylab.hist(adc[i::4], bins=opts.histbins, range=(-2**7, 2**7), normed=True)
+                add_gauss_fit(adc[i::4])
+                pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
 
-            pylab.figure(3)
-            pylab.suptitle('Core 1')
-            pylab.subplot(x_plots,y_plots,fn+1)
-            pylab.hist(adc[1::4], bins=2**5, range=(-2**7, 2**7), normed=True)
-            pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
-
-            pylab.figure(4)
-            pylab.suptitle('Core 2')
-            pylab.subplot(x_plots,y_plots,fn+1)
-            pylab.hist(adc[2::4], bins=2**5, range=(-2**7, 2**7), normed=True)
-            pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
-
-            pylab.figure(5)
-            pylab.suptitle('Core 3')
-            pylab.subplot(x_plots,y_plots,fn+1)
-            pylab.hist(adc[3::4], bins=2**5, range=(-2**7, 2**7), normed=True)
-            pylab.title('ADC values: ROACH %s, ADC %d, (ANT %d, BAND %s)'%(feng.roachhost.host,feng.adc,feng.ant,feng.band))
-
-        print 'ANT %d %s band: Signal std dev: %.3f'%(feng.ant, feng.band, np.std(adc))
+        print 'ANT %d %s band: Mean: %.3f, sigma: %.3f'%(feng.ant, feng.band, np.mean(adc), np.sqrt(np.var(adc)))
+        if opts.per_core:
+            for i in range(4):
+                print '    Core %d: Mean: %.3f, sigma: %.3f'%(i, np.mean(adc[i::4]), np.sqrt(np.var(adc[i::4])))
 
     print ''
     pylab.show()
+
 
         
 
