4481ab7767ed61a53bd7ab38f2b0df6fb71e0065
AMI Operator <ami@amisdc.(none)>
2015-10-19 15:27:39 +0100

    Add noise switch enable flag and logging changes
    Add --nonoise flag to grab autocorrelations (not noise on/off differences)
    Add spectra enable call to support new firmware
    Various logging changes.

--- ami/scripts/ami_fx/corr_monitor.py
+++ ami/scripts/ami_fx/corr_monitor.py
@@ -23,6 +23,8 @@ if __name__ == '__main__':
         help='Expiry time of redis keys in seconds. Default = 30. 0 = do not expire')
     p.add_option('-m', '--monitor', dest='monitor', action='store_true', default=False,
         help='Monitor continuously')
+    p.add_option('-n', '--nonoise', dest='noise', action='store_false', default=True,
+        help='Use this flag to disable noise switching')
 
     opts, args = p.parse_args(sys.argv[1:])
     if args == []:
@@ -39,13 +41,19 @@ if __name__ == '__main__':
     corr = AMI.AmiDC(config_file=config_file, passive=True, skip_prog=True)
     time.sleep(0.1)
 
+    # enable the autocorr capture logic
+    logger.info('Turning on auto spectra capturer')
+    a = corr.all_fengs('set_auto_capture', True)
+
+
     # turn on the noise switch
-    logger.info('attempting to turn on noise switches')
-    a = corr.all_fengs('noise_switch_enable', True)
-    logger.info('noise switches activated')
+    logger.info('Setting noise switch enable to %r'%opts.noise)
+    a = corr.all_fengs('noise_switch_enable', opts.noise)
 
     grab_n = 0
+    logger.info('Grabbing a dummy spectra for array sizing')
     x = np.zeros_like(corr.all_fengs_multithread('get_spectra', autoflip=False))
+    logger.info('Beginning spectra grab loop')
     while(True):
         tic = time.time()
         spectra = corr.all_fengs_multithread('get_spectra', autoflip=False)
@@ -58,7 +66,7 @@ if __name__ == '__main__':
             corr.redis_host.set(key, d.tolist(), ex=expire_time)
         logger.info('New monitor data sent at time %.2f'%time.time())
         if opts.plot != 0:
-            x += spectra * np.abs(eq)**2
+            x += spectra #* np.abs(eq)**2
             grab_n += 1
             if grab_n == opts.plot:
                 break
